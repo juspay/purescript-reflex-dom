169c3ddf632ca9852ee669ee37bc87215adf5355
Eric Wong <ericsyw@gmail.com>
2018-03-20 21:02:35 +0800

    cleanup

--- src/Main.purs
+++ src/Main.purs
@@ -2,7 +2,7 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Control.Monad.Eff.Console (CONSOLE)
 import DOM (DOM)
 import Data.String (length)
 import FRP (FRP)
